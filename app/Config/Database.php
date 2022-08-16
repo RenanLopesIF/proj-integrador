1660d7bf9a61330213a96570c9cf92019d703429
Renan Lopes <renan.lopes@nextpage.com.br>
2022-08-16 19:41:43 -0300

    feat: config database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
+        'username' => 'root',
         'password' => '',
-        'database' => '',
+        'database' => 'if_projeto_integrador',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
