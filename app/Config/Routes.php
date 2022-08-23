6cb5d9d444c95a65bb6c4a6c871ac7d256d442a8
Renan Lopes <rfl5@aluno.ifnmg.edu.br>
2022-08-22 22:13:21 -0300

    feat: creating cadastrar/inserir route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,6 +37,7 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Login::index');
 $routes->get('/cadastrar', 'CadastroUsuario::index');
+$routes->get('/cadastrar/inserir', 'CadastroUsuario::inserir');
 
 /*
  * --------------------------------------------------------------------
