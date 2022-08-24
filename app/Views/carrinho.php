4840d54c90c6e5ae88e5004ed041a2c2b9770152
1uanss <sousasluan212@gmail.com>
2022-08-24 17:00:44 -0300

    continuando

--- app/Views/carrinho.php
+++ app/Views/carrinho.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <link rel="stylesheet" href="./assets/css/carrinho.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+    crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="carrinho/css/style.css">
+    <link rel="stylesheet" href="carrinho/css/carrinho.css">
 
     <title>Document</title>
 </head>
@@ -77,7 +79,7 @@
                         </ul>
                     </div>
                     <div class="final">
-                        <div class="valorF">
+                        <div class="valorFinal">
                             <p>valor total</p>
                             <p>$00,00</p>
                         </div>
@@ -87,7 +89,9 @@
             </div>
         </div>
     </div>
-    <footer class="rodape"></footer>
+    <footer class="rodape">
+        <h1>rodapé</h1>
+    </footer>
 </body>
 
 </html>
