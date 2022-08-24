16b306292adf4102bd33654d2322f1ac6e45461d
devamauryjunior <jobagpj@gmail.com>
2022-08-24 00:33:21 -0300

    add: tela vitrine

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -7,5 +7,6 @@ class Login extends BaseController
     public function index()
     {
         return view('login');
+        // return view('vitrine'); // depois tirar, coloquei para testar
     }
 }
---
+++ app/Controllers/Vitrine.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Vitrine extends BaseController
+{
+    public function index()
+    {
+        return view('vitrine');
+    }
+}
