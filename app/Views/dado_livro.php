c7d2222fb498e0560b1c8350c5d0285f84a80ee7
1uanss <sousasluan212@gmail.com>
2022-08-24 20:28:26 -0300

    aterações das cores

--- app/Views/dado_livro.php
+++ app/Views/dado_livro.php
@@ -7,15 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <link rel="stylesheet" href="dados_livro/css/dados.css">
-    <link rel="stylesheet" href="dados_livro/css/style.css/">
+    <link rel="stylesheet" href="dados_livros/css/dados.css">
+    <link rel="stylesheet" href="dados_livros/css/style.css">
 </head>
 
 <body>
-    <div class="modal container" tabindex="-1">
+    <div class="modal container " tabindex="-1">
         <div class="modal-dialog container-filha">
             <div class="modal-content">
-                <div class="modal-header">
+                <div class="modal-header text-bg-dark p-3">
                     <h5 class="modal-title">Livro selecionado</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
@@ -26,20 +26,19 @@
                     </div>
 
                     <div class="modal-body">
-                        <div>
-                            <ul>
-                                <li>nome:kimetsu no yaiba</li>
-                                <li>preço:24,90</li>
-                                <li>ano:2022</li>
-                                <li>autor:Koyoharu Gotōge</li>
-                            </ul>
-                        </div>
+                        <ul>
+                            <li>kimetsu no yaiba</li>
+                            <li>R$ 24,90</li>
+                            <li>2022</li>
+                            <li>Koyoharu Gotōge</li>
+                        </ul>
                     </div>
                 </div>
 
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Voltar</button>
-                    <button type="button" class="btn btn-primary bttwo">Comprar iten</button>
+                    <button type="button" class="btn btn-secondary " data-bs-dismiss="modal">Voltar</button>
+                    <button type="button" class="btn btn-warning bttwo">comprar</button>
+                    <!-- mandar botao novo -->
                 </div>
             </div>
         </div>
