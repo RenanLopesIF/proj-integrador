2454803de45974a46d494b4dc5a851ea7f823e3c
1uanss <sousasluan212@gmail.com>
2022-08-25 20:04:04 -0300

    link do modal com a tela vitrine

--- app/Views/vitrine.php
+++ app/Views/vitrine.php
@@ -5,13 +5,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="vitrine/css/style.css">
-    <link rel="stylesheet" href="vitrine/css/vitrine.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="base_url(vitrine/css/style.css)">
+    <link rel="stylesheet" href="base_url(vitrine/css/vitrine.css)">
+    <link rel="stylesheet" href="base_url(vitrine/css/dados.css)">
     <title>loja</title>
 </head>
 
 <body>
+    <!-- inicio -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog container-filha">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">livro selecionado</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="conteudo">
+                    <div class="image-holder">
+                        <img src="./assets/img/kimetsu.webp" alt="img-1">
 
+                    </div>
+
+                    <div class="modal-body">
+                        <ul>
+                            <li>kimetsu no yaiba</li>
+                            <li>R$ 24,90</li>
+                            <li>2022</li>
+                            <li>Koyoharu Gotōge</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Voltar</button>
+                    <button type="button" class="btn btn-warning bttwo">comprar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- fim -->
     <div class="vtr-navigation">
         <h2>Loja virtual</h2>
         <nav class="navbar">
@@ -23,30 +57,16 @@
             </ul>
         </nav>
         <a href="#">
-            <img class="car" src="vitrine/img/car_cmpra.png" alt="">
+            <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
+                <path d="M14.35 43.95q-1.5 0-2.55-1.05-1.05-1.05-1.05-2.55 0-1.5 1.05-2.55 1.05-1.05 2.55-1.05 1.5 0 2.55 1.05 1.05 1.05 1.05 2.55 0 1.5-1.05 2.55-1.05 1.05-2.55 1.05Zm20 0q-1.5 0-2.55-1.05-1.05-1.05-1.05-2.55 0-1.5 1.05-2.55 1.05-1.05 2.55-1.05 1.5 0 2.55 1.05 1.05 1.05 1.05 2.55 0 1.5-1.05 2.55-1.05 1.05-2.55 1.05Zm-22.6-33 5.5 11.4h14.4l6.25-11.4Zm-1.5-3H39.7q1.15 0 1.75 1.05.6 1.05 0 2.1L34.7 23.25q-.55.95-1.425 1.525t-1.925.575H16.2l-2.8 5.2h24.55v3h-24.1q-2.1 0-3.025-1.4-.925-1.4.025-3.15l3.2-5.9L6.45 7h-3.9V4H8.4Zm7 14.4h14.4Z" />
+            </svg>
         </a>
     </div>
-
-    <!-- <nav class="navbar">
-        <div class="nav-right">
-            <li><a href="">Home</a></li>
-            <li><a href="">Produtos</a></li>
-            <li><a href="">Login</a></li>
-            <li><a href="">criar conta</a></li>
-        </div>
-        <div class="confirm">
-            <a href="#">
-                <img class="car" src="./assets/img/car_cmpra.png" alt="">
-            </a>
-        </div>
-    </nav> -->
     <div class="section-1">
         <div class="vtr-text-principal">
             <h1>Seja bem vindo</h1>
             <h2>Seus livos preferidos aqui.</h2>
             <p>Aqui tem para todos os estilos.</p>
-            <!-- <p>Caso queira doar livros</p> -->
-            <!-- <a href="" class="btn">Explore Here  <i class="fa fa-arrow-right" aria-hidden="true"></i> </a> -->
         </div>
     </div>
     <div>
@@ -54,85 +74,89 @@
 
         <div class="section-2">
             <h2>Estante principal</h2>
-            <!-- <a href="#"><img src="./images/img_2.jpg" alt=""> -->
             <div class="gallery">
-                <div class="image-holder">
-                    <a href="#">
-                        <img src="./assets/img/img_escola.jpg" alt="img-1">
-                    </a>
+                <!-- <div class="image-holder">
+                <div class="botaoCompra">
                 </div>
-                <div class="image-holder">
-                    <a href="#">
-                        <img src="./assets/img/kimetsu.webp" alt="img-1">
-                    </a>
-                    <div class="info">
-                        <ul>
-                            <div>
-                                <li>24,90</li>
-                                <li>kimetsu no yaiba</li>
-                            </div>
-
-
-                            <li>ação</li>
-                            <li>200 paginas</li>
-                            <li>capa comum</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="image-holder">
-                    <a href="#">
-                        <img src="./assets/img/kimetsu.webp" alt="img-1">
-                    </a>
-                    <!-- <p>palavra</p><p>24$</p> -->
-                </div>
-                <div class="image-holder">
-                    <a href="#">
-                        <img src="./assets/img/kimetsu.webp" alt="img-1">
-                    </a>
-                </div>
-                <div class="image-holder">
-                    <a href="#">
-                        <img src="./assets/img/kimetsu.webp" alt="img-1">
-                    </a>
-                </div>
-                <div class="image-holder">
-                    <a href="#">
-                        <img src="./assets/img/kimetsu.webp" alt="img-1">
-                    </a>
-                </div>
-                <div class="image-holder">
-                    <a href="#">
-                        <img src="./assets/img/kimetsu.webp" alt="img-1">
-                    </a>
-                </div>
-                <div class="image-holder">
-                    <a href="#">
-                        <img src="./assets/img/kimetsu.webp" alt="img-1">
-                    </a>
-                </div>
-                <div class="image-holder">
-                    <a href="#">
-                        <img src="./assets/img/kimetsu.webp" alt="img-1">
-                    </a>
-                </div>
-                <div class="image-holder">
-                    <a href="#">
-                        <img src="./assets/img/kimetsu.webp" alt="img-1">
-                    </a>
-                </div>
-                <div class="image-holder">
-                    <a href="#">
-                        <img src="./assets/img/kimetsu.webp" alt="img-1">
-                    </a>
-                </div>
-                <div class="image-holder">
-                    <a href="#">
-                        <img src="./assets/img/kimetsu.webp" alt="img-1">
-                    </a>
-                </div>
+                <button type="button" class="btn  btn-openModal" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <img src="./assets/img/img_escola.jpg" alt="img-1">
+                  </button>
+            </div>
+            <div class="image-holder">
+                <a href="#">
+                    <img src="./assets/img/kimetsu.webp" alt="img-1">
+                </a>
+            </div>
+             <div class="image-holder">
+                <a href="#">
+                    <img src="./assets/img/kimetsu.webp" alt="img-1">
+                </a>
+            </div>
+           <div class="image-holder">
+                <a href="#">
+                    <img src="./assets/img/kimetsu.webp" alt="img-1">
+                </a>
+            </div>
+            <div class="image-holder">
+                <a href="#">
+                    <img src="./assets/img/kimetsu.webp" alt="img-1">
+                </a>
+            </div>
+            <div class="image-holder">
+                <a href="#">
+                    <img src="./assets/img/kimetsu.webp" alt="img-1">
+                </a>
+            </div>
+            <div class="image-holder">
+                <a href="#">
+                    <img src="./assets/img/kimetsu.webp" alt="img-1">
+                </a>
+            </div>
+            <div class="image-holder">
+                <a href="#">
+                    <img src="./assets/img/kimetsu.webp" alt="img-1">
+                </a>
+            </div>
+            <div class="image-holder">
+                <a href="#">
+                    <img src="./assets/img/kimetsu.webp" alt="img-1">
+                </a>
+            </div>
+            <div class="image-holder">
+                <a href="#">
+                    <img src="./assets/img/kimetsu.webp" alt="img-1">
+                </a>
+            </div>
+            <div class="image-holder">
+                <a href="#">
+                    <img src="./assets/img/kimetsu.webp" alt="img-1">
+                </a>
+            </div>
+            <div class="image-holder">
+                <a href="#">
+                    <img src="./assets/img/kimetsu.webp" alt="img-1">
+                </a>
+            </div>
+            <div class="image-holder">
+                <a href="#">
+                    <img src="./assets/img/kimetsu.webp" alt="img-1">
+                </a>
+            </div>
+            <div class="image-holder">
+                <a href="#">
+                    <img src="./assets/img/kimetsu.webp" alt="img-1">
+                </a>
+            </div> -->
             </div>
 
         </div>
+        <script src="assets/bootstrap/bootstrap.bundle.min.js" class="img-fluid"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+        <!-- <script src="./assets/js/scrip.js"></script> -->
+        <script>
+            // var bt = document.querySelectorAll("#image-holder");
+        </script>
 </body>
 
 </html>
