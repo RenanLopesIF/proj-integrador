c272349edd4802c9a48ddbfa9af1ecd7661a141a
1uanss <sousasluan212@gmail.com>
2022-08-28 17:25:09 -0300

    finalização tela-carrinho

--- app/Views/carrinho.php
+++ app/Views/carrinho.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
     crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <link rel="stylesheet" href="./assets/css/carrinho.css">
+    <link rel="stylesheet" href="<?= base_url('/carrinho/css/style.css') ?> ">
+    <link rel="stylesheet" href="<?= base_url('/carrino/css/carrinho.css') ?> ">
 
     <title>Document</title>
 </head>
@@ -20,13 +20,13 @@
             <div class="carrinho">
                 <div class="sessao1">
                     <div class="txt1">
-                        <h1>meu carrinho</h1>
-                        <p>produto</p>
+                        <h1>Meu carrinho</h1>
+                        <p>Produto</p>
                     </div>
                     <div class="txt2">
-                        <p>quantidade</p>
-                        <p>tempo de entrega</p>
-                        <p>preço</p>
+                        <p>Quantidade</p>
+                        <p>Tempo de entrega</p>
+                        <p>Preço</p>
                     </div>
                 </div>
                 <div class="sessao2">
@@ -34,15 +34,15 @@
                         <div class="imagem">
                             <img src="./assets/img/kimetsu.webp" alt="">
                             <div class="txt3">
-                                <p>nome do produto</p>
-                                <h2>caracteristicas do produto</h2>
+                                <p>Nome do produto</p>
+                                <h2>Caracteristicas do produto</h2>
                             </div>
                         </div>
                         <div class="unidadeLivro">
                             <div class="btCompra">
-                                <button class="menor">-</button>
-                                <span>0</span>
-                                <button class="maior">+</button>
+                                <button class="menor" id="menor">-</button>
+                                <span id="quantityBooks">0</span>
+                                <button class="maior" id="maior">+</button>
                             </div>
                             <p>0 dias</p>
                             <p>$00,00</p>
@@ -50,13 +50,13 @@
                     </div>
                 </div>
 
-                <div class="sessao2">
+                <!-- <div class="sessao2">
                     <div class="infoLivro">
                         <div class="imagem">
                             <img src="./assets/img/kimetsu.webp" alt="">
                             <div class="txt3">
-                                <p>nome do produto</p>
-                                <h2>caracteristicas do produto</h2>
+                            <p>nome do produto</p>
+                            <h2>caracteristicas do produto</h2>
                             </div>
                         </div>
                         <div class="unidadeLivro">
@@ -69,26 +69,29 @@
                             <p>$00,00</p>
                         </div>
                     </div>
-                </div>
+                </div> -->
 
                 <div class="sessao3">
                     <div class="frete">
-                        <p>calcule frete</p>
-                        <input type="number" name="" id="" placeholder="000000-000">
+                        <p>Calcule frete</p>
+                        <div class="cepOk">
+                            <input type="number" class="confirm" name="" id="" placeholder="000000-000">
+                            <input type="button" value="ok" class="confirm" placeholder="ok">
+                        </div>
                     </div>
-                    <input type="button" value="ok" class="confirm" placeholder="ok">
                 </div>
             </div>
+
             <div class="resumo">
                 <div class="containerBorda">
                     <div class="rFinal">
-                        <p>resumo do pedido</p>
+                        <p>Resumo do pedido</p>
                     </div>
                     <div class="dadosFinal">
                         <ul>
-                            <li>valor do pedido</li>
-                            <li>frete</li>
-                            <li>desconto</li>
+                            <li>Valor do pedido</li>
+                            <li>Frete</li>
+                            <li>esconto</li>
                         </ul>
                         <ul>
                             <li>$00,00</li>
@@ -98,7 +101,7 @@
                     </div>
                     <div class="final">
                         <div class="valorF">
-                            <p>valor total</p>
+                            <p>Valor total</p>
                             <p>$00,00</p>
                         </div>
                         <input type="button" value="finalizar" placeholder="finalizar compra">
@@ -106,10 +109,12 @@
                 </div>
             </div>
         </div>
+
+        <footer class="rodape">
+            <h1>Rodapé</h1>
+        </footer>
     </div>
-    <footer class="rodape">
-        <h1>rodapé</h1>
-    </footer>
+    <script src="./assets/js/main.js"></script>
 </body>
 
 </html>
