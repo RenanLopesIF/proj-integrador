ea69b4c1aa3487b9083c20a8d8fdad2e08583a7e
1uanss <sousasluan212@gmail.com>
2022-09-04 13:42:37 -0300

    dta  base

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'integrador_if',
+        'database' => 'projeto_integrador_if',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
