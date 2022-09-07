fe479aff491688b9b1af8ed189db9a579e12b207
Renan Lopes <rfl5@aluno.ifnmg.edu.br>
2022-09-07 12:37:38 -0300

    feat: tela pagamento implementada

--- app/Controllers/Pagamento.php
+++ app/Controllers/Pagamento.php
@@ -13,4 +13,27 @@ class Pagamento extends BaseController
 
         return view('pagamento');
     }
+
+    public function confirmaPagamento()
+    {
+        $session = session();
+        if ($session->get('email') == null) {
+            return redirect('/');
+        }
+
+        $comprasModel = new \App\Models\ComprasModel();
+        $dueDate = time() + (6 * 24 * 60 * 60);
+        $status = 'pendente';
+        if ($_POST['forma_pgt'] == 1) $status = 'pago';
+
+        $res = $comprasModel->finalizarCompra([
+            'id_usuario' => $session->get('id'),
+            'forma_pgt' => $_POST['forma_pgt'],
+            'parcelas' => $_POST['parcelas'],
+            'vencimento' => date('Y-m-d', $dueDate),
+            'status' => $status
+        ]);
+
+        return redirect('carrinho');
+    }
 }
