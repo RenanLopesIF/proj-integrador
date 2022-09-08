d53a38b548f90bf385680f4e2b48f9ae4e142611
Renan Lopes <rfl5@aluno.ifnmg.edu.br>
2022-09-08 10:20:46 -0300

    refactor: padronizando arquivos css

--- app/Views/editar_livro.php
+++ app/Views/editar_livro.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="<?= base_url('/img/livros.png') ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?= base_url('/Editar_livro/css/Editar_livros.css') ?>">
-    <link rel="stylesheet" href="<?= base_url('/Editar_livro/css/style.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('/editar_livro/css/editar_livro.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('/style.css') ?>">
     <title>Editar livro</title>
 </head>
 
